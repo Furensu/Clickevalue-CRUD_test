d15139af572d1f4d392b057aab00d8602ad01b85
Furensu <uenokasai@hotmail.com>
2019-03-22 13:21:25 -0300

    Created unitial draft for the index page and user class.

---
+++ user.php
@@ -0,0 +1,52 @@
+<? php 
+    class User {
+        
+        private $userName;
+        private $userPass;
+        private $userMail;
+
+        function _construct($userN,$userP,$userM){
+                $this->setUserName($userN);
+                $this->setUserPass($userP);
+                $this->setUserMail($userM);
+        }
+
+        public function getUserName()
+        {
+                return $this->userName;
+        } 
+
+        public function setUserName($userName)
+        {
+                $this->userName = $userName;
+
+                return $this;
+        } 
+
+        public function getUserPass()
+        {
+                return $this->userPass;
+        }
+
+        public function setUserPass($userPass)
+        {
+                $this->userPass = $userPass;
+
+                return $this;
+        }
+
+        public function getUserMail()
+        {
+                return $this->userMail;
+        } 
+
+        public function setUserMail($userMail)
+        {
+                $this->userMail = $userMail;
+
+                return $this;
+        }
+        
+    }
+
+?>
